a6efeb63121b4009daac47179ffcac6940e3f924
gahee <930_10@naver.com>
2024-04-08 15:09:35 +0900

    🎉 begin project

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@utils", replacement: "/src/utils" },
+      { find: "@state", replacement: "/src/state" },
+      { find: "@query", replacement: "/src/query" },
+
+    ],
+  },
+})
