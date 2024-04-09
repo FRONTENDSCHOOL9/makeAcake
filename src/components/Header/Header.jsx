0150bb33c4667b199ccc1e33df437d9c74815852
happinessPooh <ecne3012@naver.com>
2024-04-09 22:03:14 +0900

    ✨ Feat: 메뉴 클릭 시 간단한 조건문 랜더링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
+import {useState} from "react";
+
 import Menu from "./Menu.jsx";
 import {StyledHeader, StyledNav, StyledHome} from "./styles/HeaderStyles.js";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  function handleOpenMenu() {
+    setOpen(prev => !prev);
+  } 
 
     return (
         <StyledHeader>
-          <StyledNav>
-
-          </StyledNav>
-          <StyledHome>
-
-          </StyledHome>
-
-          <Menu/>
-
-          
-
+          <StyledNav onClick={handleOpenMenu} />
+          {open && <Menu />}
+          <StyledHome />
         </StyledHeader>
-
     );
 };
