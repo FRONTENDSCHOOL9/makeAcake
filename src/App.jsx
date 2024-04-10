a73247fa8c4e556642f8e93862a20f9ca1ec8ed3
happinessPooh <ecne3012@naver.com>
2024-04-10 15:11:09 +0900

    💄 Design: MyPage에 Gnb를 배치

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import GlobalStyles from "@styles/GlobalStyle.js";
 import Header from "@components/Header/Header.jsx";
 import MyPage from "@pages/MyPage/MyPage.jsx";
+import Browse from "@pages/Browse/Browse";
 
 function App() {
 
