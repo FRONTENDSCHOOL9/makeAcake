50d97ae9f4febc051d7a184657fc4eae391f4254
gahee <930_10@naver.com>
2024-04-12 00:54:03 +0900

    🚧 상세 페이지 작업중

---
+++ src/pages/Detail/Detail.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import likeDisabled from "@assets/images/like_disabled.svg"
+import Button from "@components/Button/Button";
+import { StyledDetail } from "@pages/Detail/styles/DetailStyles";
+
+function Detail() {
+  const [ cakeInfo, setCakeInfo ] = useState({});
+  const [ like, setLike ] = useState(0);
+  const placeholderImageUrl = 'https://via.placeholder.com/360';
+
+  useEffect (() => {
+    const fakeCake = {
+      "_id": 2,
+      "seller_id": 2,
+      "price": 17260,
+      "shippingFees": 2500,
+      "show": true,
+      "active": true,
+      "name": "사조참치케이크",
+      "quantity": 200,
+      "buyQuantity": 198,
+      "address": "서울시 강남구 역삼동 123",
+      "sns": "instagram.com/sajotuna",
+      "mainImages": [
+        {
+          "path": "https://via.placeholder.com/360",
+          "name": "sample-diver.jpg",
+          "originalname": "cake.jpg"
+        }
+      ],
+      "content": "맛있는 초코케이크 맛있어요. 간단한 설명 예시 요약입니다. 맛있는 사조참치케이크 맛있는 초코케이크 맛있어요. 간단한 설명 예시 요약입니다. 맛있는 사조참치케이크",
+      "createdAt": "2024.03.04 09:54:38",
+      "updatedAt": "2024.03.09 14:59:38",
+      "extra": {
+        "isNew": false,
+        "isBest": true,
+        "sort": 4
+      },
+      "workingday": {
+        "day": "화요일",
+        "start": 10,
+        "end": 17
+      },
+
+    };
+
+    setCakeInfo({
+      ...fakeCake,
+      mainImages: {
+        ...fakeCake.mainImages,
+      },
+      extra: {
+        ...fakeCake.extra,
+      },
+      workingday: {
+        ...fakeCake.workingday
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <img src={placeholderImageUrl} alt="cakeImage" />
+      <StyledDetail>
+        <h3>{ cakeInfo.name }</h3>
+        <div>
+          <p>영업시간 <span>{ cakeInfo.workingday?.start }시 ~ { cakeInfo.workingday?.end }시</span></p>
+          <p>위치 <span>{ cakeInfo.address }</span></p>
+          <p>SNS <span>{ cakeInfo.sns }</span></p>
+        </div>
+        <p>{ cakeInfo.content }</p>
+        <div>
+          <button type="button" onClick={ ()=> {setLike(like + 1)} }><img src={likeDisabled} alt="찜 추가"/></button>
+          <Button>예약하기</Button>
+        </div>
+      </StyledDetail>
+
+    </div>
+  )
+}
+
+export default Detail;
---
+++ src/pages/Detail/styles/DetailStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const StyledDetail = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  & > h3 {
+    font-size: var(--font-size-large);
+    font-family: PretendardWoffSemiBold, sans-serif;
+  }
+`
