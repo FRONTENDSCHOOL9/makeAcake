f6805eb68e98c3fa1e9289ad1950fc89887b8e51
gahee <930_10@naver.com>
2024-04-15 17:11:06 +0900

    🚧  Design: ReservationPage 진행중

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -1,15 +1,36 @@
 import Button from "@components/Button/Button";
 import AppCalendar from "@components/Calendar/AppCalendar";
-import GenerateTimeTable from "@pages/Reservation/TimeTable";
+import GenerateTimeTable from "@pages/Reservation/GenerateTimeTable";
 import { ReservationSection, StyledReservation } from "@pages/Reservation/styles/ReservationStyles";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Reservation() {
-
+  const [ cake, setCake ] = useState(null);
+  const [ selectedOption, setSelectedOption ] = useState(null);
+  const [ selectedTime, setSelectedTime ] = useState(null);
   const placeholderImageUrl = 'https://via.placeholder.com/360';
   useEffect(()=> {
+    const fakeCake = {
+      name: '사조참치케이크', 
+      price: {
+        mini: 20000,
+        1: 30000,
+        2: 40000,
+        3: 50000
+      }
+    }
+    setCake({ ...fakeCake, price: {...fakeCake.price} });
+  }, []);
 
-  }, [])
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleTimeClick = (time) => {
+    setSelectedTime(time);
+  };
+
+  if(!cake || !cake.price) return null;
 
   return (
     <StyledReservation>
@@ -18,44 +39,54 @@ function Reservation() {
       </div>
       <img className="cakeImage" src={placeholderImageUrl} alt="cakeImage" />
       <div className="main">
-        <h3>사조참치케이크</h3>
+        <h3>{ cake.name }</h3>
         <ReservationSection>
-          <div className="main-message">
-            <h4>🚨 예약 주의 사항</h4>
-            <p className="main-message-content">5분 이상 지각 시 케이크 폐기</p>
-          </div>
           <div className="main-time">
             <h4>1. 픽업 날짜와 시간을 선택해 주세요.</h4>
             <AppCalendar />
             <div className="time-title">오전</div>
             <ul>
-              <GenerateTimeTable startTime={"2021-01-01T09:30:00"} endTime={"2021-01-01T11:30:00"}/>  
+              <GenerateTimeTable startTime={"2021-01-01T09:30:00"} endTime={"2021-01-01T11:30:00"} handleTimeClick={handleTimeClick} selectedTime={selectedTime} />  
             </ul>
             <div className="time-title">오후</div>
             <ul>
-              <GenerateTimeTable startTime={"2021-01-01T12:00:00"} endTime={"2021-01-01T21:30:00"}/>  
+              <GenerateTimeTable startTime={"2021-01-01T12:00:00"} endTime={"2021-01-01T21:30:00"} handleTimeClick={handleTimeClick} selectedTime={selectedTime}/>  
             </ul>
           </div>
           <div className="main-form">
-            <h4>2. 예약 정보를 기입해 주세요.</h4>
-            <div>
-
-            </div>
-            <div>
-              <label>3. 레터링 정보</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>4. 요청 사항</label>
-              <input type="text" />
-            </div>
-
-          </div>
-          <div className="main-reserveInfo">
-            3. 예약자 정보 확인
+            <h4>2. 예약 정보를 확인해 주세요.</h4>
+            <form>
+              <div>
+                <label htmlFor="size">1. 사이즈를 선택해 주세요.</label>
+                <div className="cake-option">
+                  {Object.keys(cake.price).map((option, index) => (
+                    <button
+                      key={index}
+                      className={selectedOption === option ? 'cake-option-btn selected' : 'cake-option-btn'}
+                      onClick={() => handleOptionClick(option)}
+                      type="button"
+                    >
+                      {option !== 'mini' ? `${option}호` : '미니도시락'}
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <label htmlFor="lettering">2. 레터링 정보를 기입해 주세요.</label>
+                <input type="text" id="lettering"/>
+              </div>
+              <div>
+                <label htmlFor="request">3. 요청 사항이 있다면 남겨주세요.</label>
+                <input type="text" id="request"/>
+              </div>
+              <div>
+                <label htmlFor="pickupName">4. 픽업하시는 분 성함을 입력해 주세요.</label>
+                <input type="text" id="pickupName" />
+              </div>             
+            </form>
           </div>
           <div className="main-check">
-            4. 동의
+            <label htmlFor="check">3. 예약 확정 전 꼭 확인해 주세요.</label>
             <input type="checkbox" id="main-check-content"/>
           </div>
         </ReservationSection>
--- src/pages/Reservation/styles/ReservationStyles.js
+++ src/pages/Reservation/styles/ReservationStyles.js
@@ -2,15 +2,18 @@ import styled from "styled-components";
 
 
 export const StyledReservation = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   .header {
     display: flex;
     justify-content: center;
-      & > h2 {
-        font-family: PretendardWoffSemiBold, sans-serif;
-        font-size: var(--font-size-xlarge);
-        margin: 30px 0;
-        padding: 0 20px;
-      }
+    & > h2 {
+      font-family: PretendardWoffSemiBold, sans-serif;
+      font-size: var(--font-size-xlarge);
+      margin: 30px 0;
+      padding: 0 20px;
+    }
   }
 
   .cakeImage {
@@ -21,10 +24,12 @@ export const StyledReservation = styled.div`
   .main {
     box-shadow: inset 0 0 20px red;
     padding: 30px 20px;
-      & > h3 {
-        box-shadow: inset 0 0 20px purple;
-        font-size: var(--font-size-xlarge);
-      }
+    width: 360px;
+    box-sizing: border-box;
+    & > h3 {
+      box-shadow: inset 0 0 20px purple;
+      font-size: var(--font-size-xlarge);
+    }
   }
 `
 
@@ -35,15 +40,46 @@ export const ReservationSection = styled.div`
   margin: 30px 0;
   gap: 30px;
   font-size: var(--font-size-large);
-    & > div {
-      box-shadow: inset 0 0 20px blue;
-      & > .main-message-content {
-        font-size: var(--font-size-medium);
+  & > .main-time {
+    & > .time-title {
+      font-size: var(--font-size-medium);
+      margin: 10px 0;
+    }
+    & > ul {
+      display: flex;
+      width: 100%;
+      flex-wrap: wrap;
+    }
+  }
+
+  & .main-form {
+    & > form {
+      padding: 5px 20px;
+      font-size: var(--font-size-medium);
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      & .cake-option {
+        display: flex;
+        gap: 10px;
       }
-      & > .time-title {
-        padding-left: 20px;
-        font-size: var(--font-size-medium);
-        margin: 10px 0;
+      & button {
+        border: 1px solid var(--primary-black-color);
+        border-radius: 10px;
+        padding: 5px 10px;
+      }
+      & button:hover, button:active {
+        background-color: var(--primary-black-color);
+        color: var(--primary-white-color);
+      }
+      & input {
+        border: 0;
+        border-bottom: 1px solid var(--primary-black-color);
+        width: 80%;
+      }
+      & input:focus {
+        outline: none;
       }
     }
+  }
 `
