4f8003bc78103d78136b9db93c9c4f48678b5d9a
Ecne <ecne3012@naver.com>
2024-04-16 16:56:48 +0900

    ⚰️ Fix: MainPage 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
+import {useState} from "react";
+
 import GlobalStyles from "@styles/GlobalStyle.js";
 import Header from "@components/Header/Header.jsx";
 import MyPage from "@pages/MyPage/MyPage.jsx";
 import Main from '@pages/Main/Main'
 import Detail from "@pages/Detail/Detail";
 import Browse from "@pages/Browse/Browse";
+import SearchBox from "@components/SearchBox/SearchBox"; 
 import './App.css'
 
 import { StyleSheetManager } from 'styled-components';
 
 function App() {
+  const shouldForwardProp = prop => prop != "isSelected";
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const shouldForwardProp = prop => prop != "isSelected";
 
+  function handleSearchOpen() {
+    console.log("searchOpen:" + searchOpen)
+    setSearchOpen(true);
+  }
+
+  function handleSearchClose() {
+    console.log("searchOpen:" + searchOpen)
+    setSearchOpen(false);
+    setMenuOpen(false);
+  }
+
+  function handleMenuToggle() {
+    setMenuOpen(prev => !prev);
+  } 
 
   return (
     <StyleSheetManager shouldForwardProp={shouldForwardProp}>
       <GlobalStyles />
-      <Header/>
-      {/* <MyPage/> */}
-      {/* <Main/> */}
-      {/* <Detail/> */}
-      <MyPage/>
+      <Header onSearch={handleSearchOpen} searchOpen={searchOpen} onMenu={handleMenuToggle}  menuOpen={menuOpen}/>
+      {searchOpen && <SearchBox onClose={handleSearchClose}/>}
+      <Main />
     </StyleSheetManager>
   )
 }
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
+import {useState} from "react"
 
 import {StyledMenu, StyledMenuContent} from "./styles/HeaderStyles.js"
 
-export default function Menu({open}) {
+export default function Menu({onSelect}) {
+
   return (
-    <StyledMenu isOpen={open}>
-      <StyledMenuContent>SEARCH</StyledMenuContent>
+    <StyledMenu>
+      <StyledMenuContent onClick={onSelect}>SEARCH</StyledMenuContent>
       <StyledMenuContent>BROWSE</StyledMenuContent>
       <StyledMenuContent>WISHLIST</StyledMenuContent>
       <StyledMenuContent>MY PAGE</StyledMenuContent>
