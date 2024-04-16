7e2a32daf87bef33010f09785d6ec27f4a9d7756
Ecne <ecne3012@naver.com>
2024-04-16 17:37:47 +0900

    Fix: MainSection Card의 key값 수정

--- src/pages/Main/components/MainSection.jsx
+++ src/pages/Main/components/MainSection.jsx
@@ -2,13 +2,13 @@ import {StyledList} from "@pages/Main/styles/MainStyles";
 import Card from "@components/Card/Card.jsx";
 
 export default function MainSection({children, cakes}) {
-    const location ="main"
+    const location = "main";
     return (
         <StyledList>
           <h3>{children}</h3>
           <ul className="card-container">
-            { cakes.slice(0,6).map(cake => (
-                <Card key={cake.name} cake={cake} location={location}/>
+            { cakes.slice(0,6).map((cake, index) => (
+                <Card key={index} cake={cake} location={location}/>
             )) }
           </ul>
         </StyledList>
