d6da9b7e504719dd6798490081da41fd3b99e5e9
gahee <930_10@naver.com>
2024-04-19 11:17:35 +0900

    🐛 FIx: 상품 상세 조회 시 상위 상품 조회 불가 오류 수정

--- api/dbinit/team/data.js
+++ api/dbinit/team/data.js
@@ -71,7 +71,7 @@ export const initData = async (nextSeq) => {
           category: ['type', 'type_lettering','taste', 'taste_choco'] // 모양 > 레터링, 맛 > 초코
         },
         show: true,
-        seller_id: await nextSeq('user'),
+        seller_id: 1,
         active: true,
         createdAt: "2024.04.17 15:05:21",
         updatedAt: "2024.04.17 15:05:21",
@@ -219,26 +219,26 @@ export const initData = async (nextSeq) => {
           }
         ],
         content: `
-              <div class="product-content">
-                <p>
-                  짱구 캐릭터 케이크 - 딸기 시트 + 가나슈 & 딸기크림 맛입니다.
-                  모든 짱구 캐릭터 케이크의 겉면은 크림치즈버터로 동일합니다.
-                </p>
-                <p>
-                  너무 귀여워서 요즘 인기 급부상한 케이크 디자인이에요~
-                  케이크 박스를 열자마자 웃음이 터질만큼 귀여워요!
-                </p>
-                <p>
-                  size별 가격대는 상이합니다. 유심히 보시고 선택해주세요!
-                </p>
-                <ul>
-                  <li>도시락 케이크: 지름 약 10cm - 1.5인분</li>
-                  <li>미니 케이크: 지름 약 12cm - 2인분</li>
-                  <li>1호: 지름 약 15cm - 3-4인분</li>
-                  <li>2호: 지름 약 20cm - 5인분 이상</li>
-                </ul>
-              </div>
-          `,
+          <div class="product-content">
+            <p>
+              짱구 캐릭터 케이크 - 딸기 시트 + 가나슈 & 딸기크림 맛입니다.
+              모든 짱구 캐릭터 케이크의 겉면은 크림치즈버터로 동일합니다.
+            </p>
+            <p>
+              너무 귀여워서 요즘 인기 급부상한 케이크 디자인이에요~
+              케이크 박스를 열자마자 웃음이 터질만큼 귀여워요!
+            </p>
+            <p>
+              size별 가격대는 상이합니다. 유심히 보시고 선택해주세요!
+            </p>
+            <ul>
+              <li>도시락 케이크: 지름 약 10cm - 1.5인분</li>
+              <li>미니 케이크: 지름 약 12cm - 2인분</li>
+              <li>1호: 지름 약 15cm - 3-4인분</li>
+              <li>2호: 지름 약 20cm - 5인분 이상</li>
+            </ul>
+          </div>
+        `,
         extra: {
           depth: 1,
           category: ['type', 'type_charactor','taste', 'taste_strawberry'] // 모양 > 캐릭터, 맛 > 딸기
@@ -259,124 +259,123 @@ export const initData = async (nextSeq) => {
           createdAt: "2024.04.17 23:06:11",
           updatedAt: "2024.04.17 23:06:11"
         },
-        options: {
-          item: [
-            {
-              _id: await nextSeq('product'),
-              price: 20000,
-              quantity: 3,
-              name: "짱구 캐릭터 케이크 - 딸기 도시락",
-              extra: {
-                depth: 2,
-                parent: 5,
-                size: "10cm",
-
-              },
-              show: true,
-              seller_id: 1,
-              active: true,
-              buyQuantity: 0,
-              createdAt: "2024.04.17 16:43:26",
-              updatedAt: "2024.04.17 16:43:26",
-              seller: {
-                _id: 1,
-                email: "seller@market.com",
-                name: "판매자",
-                phone: "01011112222",
-                address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
-                type: "seller",
-                loginType: "email",
-                createdAt: "2024.04.17 23:06:11",
-                updatedAt: "2024.04.17 23:06:11"
-              }
-            },
-            {
-              _id: await nextSeq('product'),
-              price: 30000,
-              quantity: 1,
-              name: "짱구 캐릭터 케이크 - 딸기 미니 케이크",
-              extra: {
-                depth: 2,
-                parent: 5,
-                size: "12cm"
-              },
-              show: true,
-              seller_id: 1,
-              active: true,
-              buyQuantity: 0,
-              createdAt: "2024.04.17 16:43:26",
-              updatedAt: "2024.04.17 16:43:26",
-              seller: {
-                _id: 1,
-                email: "seller@market.com",
-                name: "판매자",
-                phone: "01011112222",
-                address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
-                type: "seller",
-                loginType: "email",
-                createdAt: "2024.04.17 23:06:11",
-                updatedAt: "2024.04.17 23:06:11"
-              }
-            },
-            {
-              _id: await nextSeq('product'),
-              price: 45000,
-              quantity: 3,
-              name: "짱구 캐릭터 케이크 - 딸기 1호 케이크",
-              extra: {
-                depth: 2,
-                parent: 5,
-                size: "15cm"
-              },
-              show: true,
-              seller_id: 1,
-              active: true,
-              buyQuantity: 0,
-              createdAt: "2024.04.17 16:43:26",
-              updatedAt: "2024.04.17 16:43:26",
-              seller: {
-                _id: 1,
-                email: "seller@market.com",
-                name: "판매자",
-                phone: "01011112222",
-                address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
-                type: "seller",
-                loginType: "email",
-                createdAt: "2024.04.17 23:06:11",
-                updatedAt: "2024.04.17 23:06:11"
-              }
-            },
-            {
-              _id: await nextSeq('product'),
-              price: 50000,
-              quantity: 3,
-              name: "짱구 캐릭터 케이크 - 딸기 2호 케이크",
-              extra: {
-                depth: 2,
-                parent: 5,
-                size: "20cm"
-              },
-              show: true,
-              seller_id: 1,
-              active: true,
-              buyQuantity: 0,
-              createdAt: "2024.04.17 16:43:26",
-              updatedAt: "2024.04.17 16:43:26",
-              seller: {
-                _id: 1,
-                email: "seller@market.com",
-                name: "판매자",
-                phone: "01011112222",
-                address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
-                type: "seller",
-                loginType: "email",
-                createdAt: "2024.04.17 23:06:11",
-                updatedAt: "2024.04.17 23:06:11"
-              }
-            }
-          ]
-        },
       },
+      {
+        _id: await nextSeq('product'),
+        price: 20000,
+        quantity: 3,
+        name: "짱구 캐릭터 케이크 - 딸기 도시락",
+        extra: {
+          depth: 2,
+          parent: 5,
+          size: "10cm",
+          category: ["size", "size_xsmall"]
+        },
+        show: true,
+        seller_id: 1,
+        active: true,
+        buyQuantity: 0,
+        createdAt: "2024.04.17 16:43:26",
+        updatedAt: "2024.04.17 16:43:26",
+        seller: {
+          _id: 1,
+          email: "seller@market.com",
+          name: "판매자",
+          phone: "01011112222",
+          address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
+          type: "seller",
+          loginType: "email",
+          createdAt: "2024.04.17 23:06:11",
+          updatedAt: "2024.04.17 23:06:11"
+        }
+      },
+      {
+        _id: await nextSeq('product'),
+        price: 30000,
+        quantity: 1,
+        name: "짱구 캐릭터 케이크 - 딸기 미니 케이크",
+        extra: {
+          depth: 2,
+          parent: 5,
+          size: "12cm",
+          category: ["size", "size_mini"]
+        },
+        show: true,
+        seller_id: 1,
+        active: true,
+        buyQuantity: 0,
+        createdAt: "2024.04.17 16:43:26",
+        updatedAt: "2024.04.17 16:43:26",
+        seller: {
+          _id: 1,
+          email: "seller@market.com",
+          name: "판매자",
+          phone: "01011112222",
+          address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
+          type: "seller",
+          loginType: "email",
+          createdAt: "2024.04.17 23:06:11",
+          updatedAt: "2024.04.17 23:06:11"
+        }
+      },
+      {
+        _id: await nextSeq('product'),
+        price: 45000,
+        quantity: 3,
+        name: "짱구 캐릭터 케이크 - 딸기 1호 케이크",
+        extra: {
+          depth: 2,
+          parent: 5,
+          size: "15cm",
+          category: ["size", "size_1"]
+        },
+        show: true,
+        seller_id: 1,
+        active: true,
+        buyQuantity: 0,
+        createdAt: "2024.04.17 16:43:26",
+        updatedAt: "2024.04.17 16:43:26",
+        seller: {
+          _id: 1,
+          email: "seller@market.com",
+          name: "판매자",
+          phone: "01011112222",
+          address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
+          type: "seller",
+          loginType: "email",
+          createdAt: "2024.04.17 23:06:11",
+          updatedAt: "2024.04.17 23:06:11"
+        }
+      },
+      {
+        _id: await nextSeq('product'),
+        price: 50000,
+        quantity: 3,
+        name: "짱구 캐릭터 케이크 - 딸기 2호 케이크",
+        extra: {
+          depth: 2,
+          parent: 5,
+          size: "20cm",
+          category: ["size", "size_2"]
+        },
+        show: true,
+        seller_id: 1,
+        active: true,
+        buyQuantity: 0,
+        createdAt: "2024.04.17 16:43:26",
+        updatedAt: "2024.04.17 16:43:26",
+        seller: {
+          _id: 1,
+          email: "seller@market.com",
+          name: "판매자",
+          phone: "01011112222",
+          address: "서울특별시 종로구 종로3길 17 광화문 D타워 17층",
+          type: "seller",
+          loginType: "email",
+          createdAt: "2024.04.17 23:06:11",
+          updatedAt: "2024.04.17 23:06:11"
+        }
+      }
     ],
     // 주문
     order: [
