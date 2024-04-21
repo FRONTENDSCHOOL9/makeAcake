bbb14df70ae36f8dd2cdcf47b1ae142452eb4eba
gahee <930_10@naver.com>
2024-04-21 18:15:53 +0900

    👔 react-query 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import {RecoilRoot} from "recoil";
 import "./App.css";
 
 import routes from "./routesConfig.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <RecoilRoot >
-      <RouterProvider router={router} />
-    </RecoilRoot>
+    < QueryClientProvider client={ queryClient } >
+      <RecoilRoot >
+        <RouterProvider router={router} />
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen = {false} />
+    </QueryClientProvider>
+
   )
 }
 
