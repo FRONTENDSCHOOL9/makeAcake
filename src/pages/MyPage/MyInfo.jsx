aba81ce65952e2090f213d35fb26016b7357540d
strdxtq <strdxtq@gmail.com>
2024-04-22 22:28:41 +0900

    💄 Add: Mypage 정보수정 UI 추가

--- src/pages/MyPage/MyInfo.jsx
+++ src/pages/MyPage/MyInfo.jsx
@@ -1,9 +1,36 @@
+import { MyInfoContainer } from "./styles/MyInfoStyles.js";
+
+
+
 function MyInfo () {
   return (
-    <>
-      <h1>Info Page</h1>
-    </>
+    <MyInfoContainer>
+      <h1 className="title">계정 정보</h1>
+      <div className="control">
+        <div className="control-row"><label htmlFor="email">이메일</label>
+        <input type="email" id="email" name="email" placeholder ="abc111@naver.com" disabled/></div>
+        <div className="control-row"><label htmlFor="email">비밀번호 변경</label>
+        <input type="email" id="email" name="email" placeholder ="********" /></div>
+        <div className="control-row"><label htmlFor="email">비밀번호 재확인</label>
+        <input type="email" id="email" name="email" placeholder ="********" /></div>
+        <div className="control-row"><label htmlFor="email">닉네임</label>
+        <input type="email" id="email" name="email" placeholder ="" /></div>
+      
+      <div className="control-row">
+        <label htmlFor="email">지역</label>
+        <input type="email" id="email" name="email" placeholder ="" />
+        <button type="button">주소 찾기</button>
+      </div>
+      <div className="control-row">
+        <label htmlFor="email">연락처</label>
+        <input type="email" id="email" name="email" placeholder ="" />
+        <button type="button">인증</button>
+      </div>
+      </div>
+      <button className="button" type="button">확인</button>
+    </MyInfoContainer>
   )
 }
+
 
 export default MyInfo;
