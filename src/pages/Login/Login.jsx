9707fde110082e1689e39b26ea08899fac571edb
gahee <930_10@naver.com>
2024-04-23 09:58:28 +0900

    ✨ 로그인 기능 추가

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,13 +5,16 @@ import naver from "@assets/images/naver.svg"
 import kkt from "@assets/images/kkt.svg"
 import google from "@assets/images/google.svg"
 import apple from "@assets/images/apple.svg"
-import axios from 'axios';
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import useCustomAxios from "@query/useCustomAxios.mjs";
+import { useSetRecoilState } from "recoil";
+import { memberState } from "@recoil/atoms.js";
 
 export default function Login() {
-
+  const location = useLocation();
   const navigate = useNavigate();
-
+  const axios = useCustomAxios();
+  const setUser = useSetRecoilState(memberState)
   const handleClickBackBtn = () => {
     /* 데이터 소실 가능성 높으니 recoil / 쿼리 스트링 / 로컬 스토리지 중 하나 정해서 추후에 데이터 보존해야 함 */
     navigate(-1);
@@ -25,17 +28,33 @@ export default function Login() {
     register,
     handleSubmit,
     formState: { errors },
+    setError
   } = useForm({
     values: {
+      email: '',
+      password: ''
     }
   });
 
   const onSubmit = async (formData) => {
     try {
-      const response = await axios.post('https://market-lion.koyeb.app/api/users/login', formData);
-      alert(response.data.item.name + '님 로그인에 성공했습니다.');
+      const response = await axios.post('users/login', formData);
+      // 사용자 정보 recoil에 저장
+      setUser({
+        _id: response.data.item._id,
+        name: response.data.item.name,
+        token: response.data.item.token,
+        type: response.data.item.type
+      });
+      alert(response.data.item.name + '님 로그인 되었습니다.');
+      navigate(location.state?.from || '/'); // 메인으로 이동
     } catch (error) {
-      console.error(error)
+      // AxiosError(네트워크 에러: response가 없음, 서버의 4xx, 5xx 응답 상태 코드를 받았을 때: response 있음)
+      if (error.response?.data.errors) {
+        error.response?.data.errors.forEach((error) => setError(error.path, { message: error.msg }));
+      } else if (error.response?.data.message) {
+        alert(error.response?.data.message);
+      }
     }
   };
 
@@ -49,7 +68,7 @@ export default function Login() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <Input type="email" id="email" className="email" placeholder="이메일을 입력하세요."
-           {...register('email', {
+          {...register('email', {
             required: "이메일을 입력하세요.",
             pattern: {
               value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
@@ -59,7 +78,7 @@ export default function Login() {
         </div>
         <div>
           <Input type="password" id="password" className="password" placeholder="비밀번호를 입력하세요."
-           {...register('password', {
+          {...register('password', {
             required: "비밀번호를 입력하세요.",
             minLength: {
               value: 8,
