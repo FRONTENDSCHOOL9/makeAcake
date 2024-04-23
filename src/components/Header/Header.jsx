328d0365ffa1fd0c03f87b8de6cdf68893d2b6fd
gahee <930_10@naver.com>
2024-04-23 10:56:55 +0900

    ✨ 로그아웃 기능, 버튼 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import {StyledHeader, StyledNav, StyledHome, StyledLogin} from "./styles/HeaderStyles.js";
 
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import { useHandleMenu } from "@hooks/useHandleMenu";
+import { memberState } from "@recoil/atoms.js";
+import { useRecoilState } from "recoil";
 
 
 export default function Header() {
   const {toggleMenu, closeMenu} = useHandleMenu();
+  const navigate = useNavigate();
+  const [ user, setUser ] = useRecoilState(memberState);
+
+  const handleLogout = () => {
+    setUser(null);
+    navigate('/');
+  }
 
     return (
       <>
@@ -13,6 +22,14 @@ export default function Header() {
           <StyledNav onClick={toggleMenu} />
           <StyledHome as={Link} to="/" onClick={closeMenu} />
           {/* 회원구분은 추후 개발 */}
+          {user ? (
+            <div>
+              {user.name}님,
+              <button onClick={handleLogout}>로그아웃</button>
+            </div>
+          )
+            : <button onClick={() => navigate('/login')}>로그인</button>
+          }
           <StyledLogin as={Link} to="/login"/>
         </StyledHeader>
       </>
