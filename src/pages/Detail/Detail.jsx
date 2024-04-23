f76f0593716a277e44cd742d9fcee386384a446e
gahee <930_10@naver.com>
2024-04-23 10:41:15 +0900

    🚧 디테일 페이지 수정 중

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,21 +1,20 @@
 import { useEffect, useRef, useState } from "react";
-import likeDisabled from "@assets/images/like_disabled.svg"
 import { DetailContainer, StyledDetail } from "@pages/Detail/styles/DetailStyles";
 import parse from 'html-react-parser';
 
 import { useNavigate, useParams } from "react-router";
 import useCustomAxios from "@query/useCustomAxios.mjs";
 import { useQuery } from "@tanstack/react-query";
+import { useRecoilValue } from "recoil";
+import { memberState } from "@recoil/atoms";
 
 function Detail() {
   const axios = useCustomAxios();
   const { productId } = useParams();
   const navigate = useNavigate();
-  const [ like, setLike ] = useState(0);
-
-  const handleClick = () => {
-    navigate(`/products/1/reservation`);
-  }
+  const [ isBookmarked, setIsBookmarked ] = useState(false);
+  const [ bookmark, setBookmark ] = useState(null);
+  const user = useRecoilValue(memberState);
 
   let firstRender = useRef(true);
 
@@ -36,6 +35,20 @@ function Detail() {
   const item = data?.item;
   console.log('item', item);
 
+  console.log(user);
+
+  const handleClick = () => {
+    if (!user) {
+      const gotoLogin = confirm(
+        '로그인 후 이용 가능합니다. \n 로그인 하시겠어요?'
+      );
+      gotoLogin && navigate('/login');
+    } else {
+      navigate(`/products/${productId}/reservation`);
+    }
+  }
+
+
   return (
     <DetailContainer>
       <img src={`${import.meta.env.VITE_API_SERVER}/files/${import.meta.env.VITE_CLIENT_ID}/${item.mainImage?.path}`} alt={`${item.name} 이미지`}/>
@@ -55,9 +68,9 @@ function Detail() {
             <dd>{ item.seller.email }</dd>
           </dl>
         </div>        
-        { parse(item.content) }
+        { item.content && parse(item.content) }
         <div>
-          <button type="button" onClick={ ()=> {setLike(like + 1)} }><img src={likeDisabled} alt="찜 추가"/></button>
+          {/* <button type="button" onClick={ ()=> {setLike(like + 1)} }><img src={likeDisabled} alt="찜 추가"/></button> */}
           <button onClick={handleClick}>예약하기</button> 
         </div>
       </StyledDetail>
